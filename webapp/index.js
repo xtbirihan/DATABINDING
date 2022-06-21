c203995b7512442ef2eb8b93b25e836798da0387
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-21 11:20:08 +0000

    Sample: Data Binding - Step 2 - Creating a Model

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,15 @@
 sap.ui.require([
-	"sap/m/Text"
-], function (Text) {
+	"sap/m/Text",
+    "sap/ui/model/json/JSONModel"
+], function (Text, JSONModel) {
 	"use strict";
 
 	// Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
+        var oModel = new JSONModel({
+            greetingText: "Hi, my name is Harry Hawk"
+        });
+        sap.ui.getCore().setModel();
 		// Create a text UI element that displays a hardcoded text string
 		new Text({text: "Hi, my name is Harry Hawk"}).placeAt("content");
 	});
