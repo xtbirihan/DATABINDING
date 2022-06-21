b024b253892986b4dd485cc4a214bd58ccebb4bc
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-21 12:50:28 +0000

    Sample: Data Binding - Step 10 - Property Formatting Using Data Types

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,7 @@
 sap.ui.require([
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/core/mvc/XMLView",
-    "sap/ui/model/resource/ResourceModel"
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/XMLView",
+	"sap/ui/model/resource/ResourceModel"
 ], function (JSONModel, XMLView, ResourceModel) {
 	"use strict";
 
@@ -16,18 +16,22 @@ sap.ui.require([
 				city: "Walldorf",
 				zip: "69190",
 				country: "Germany"
-			}            
+			},
+			salesAmount: 12345.6789,
+			currencyCode: "EUR"
+
 		});
+
 		// Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
 
-        var oResourceModel = new ResourceModel({
-            bundleName: "sap.ui.demo.db.i18n.i18n",
-            supportedLocales: ["", "de"],
-            fallbackLocale: ""
-        });
+		var oResourceModel = new ResourceModel({
+			bundleName: "sap.ui.demo.db.i18n.i18n",
+			supportedLocales: ["", "de"],
+			fallbackLocale: ""
+		});
 
-        sap.ui.getCore().setModel(oResourceModel, "i18n");
+		sap.ui.getCore().setModel(oResourceModel, "i18n");
 
 		// Display the XML view called "App"
 		new XMLView({
